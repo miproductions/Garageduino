0ca863f7ff8541b775034197ea7f0a15fb05a02b
James Radvan <james@radsy.com>
2015-08-08 16:24:27 +1000

    refactored debugging

--- GarageDuinoV1/GarageDuinoV1.ino
+++ GarageDuinoV1/GarageDuinoV1.ino
@@ -1,31 +1,33 @@
 /*
-HC-SR04 Ping distance sensor]
-VCC to arduino 5v GND to arduino GND
-Echo to Arduino pin 13 Trig to Arduino pin 12
-Red POS to Arduino pin 11
-Green POS to Arduino pin 10
-560 ohm resistor to both LED NEG and GRD power rail
-More info at: http://goo.gl/kJ8Gl
-Original code improvements to the Ping sketch sourced from Trollmaker.com
-Some code and wiring inspired by http://en.wikiversity.org/wiki/User:Dstaub/robotcar
-*/
+ * Garage Door Sensor
+ * HC-SR04 Ultrasonic Sensor 
+ * Freetronics EtherTen Uno-compatible (presumed on 10,11,12,13 as per Ethernet Shield)
+ * Error LED
+ * OpenHAB IoT Gateway
+ */
 
-#define erPin 13
-#define trPin 10
-#define ecPin 11
-#define ping 100
-#define ok 20
-#define tmg 58.2
+#define erPin 7 // Error LED
+#define trPin 8 // HC-SR04 Trigger Pin
+#define ecPin 9 // HC-SR04 Echo Pin
+#define ping 1000 // no less than ~60ms to allow one ping to complete before the next is sent
+#define ok 20 // distance in cm under which the door is open
+#define tmg 58.2 // converts the ping duration to cm
 #define omsg " open"
 #define cmsg " closed"
 #define emsg "error"
+#define dbg true // serial debugging
+
+#include <SPI.h>
+#include <Ethernet.h>
+
 void setup() {
-  Serial.begin (9600);
+  if (dbg) Serial.begin (9600);
   pinMode(trPin, OUTPUT);
   pinMode(ecPin, INPUT);
   pinMode(erPin, OUTPUT);
   digitalWrite(erPin, HIGH);
 }
+
 void loop() {
   long dur, dist;
   digitalWrite(trPin, LOW);
@@ -38,19 +40,26 @@ void loop() {
   if (dist >= 200 || dist <= 0) {
     // Sensor isn't reading a valid value
     digitalWrite(erPin,HIGH);
-    Serial.println(emsg);
+    debug(dist, emsg);
   } else if (dist < ok) {
     // door is open
     digitalWrite(erPin,HIGH);
     delay(100);
     digitalWrite(erPin,LOW);
-    Serial.print(dist);
-    Serial.println(omsg);
+    debug(dist, omsg);
   } else {
     // door is closed
     digitalWrite(erPin,LOW);
-    Serial.print(dist);
-    Serial.println(cmsg);
+    debug(dist, cmsg);
   }
   delay(ping);
 }
+
+void debug(long dist, String msg) {
+  if (dbg) {
+    Serial.print(dist);
+    Serial.println(msg);
+  }
+}
+
+
