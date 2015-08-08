80fbbabc2bab665950c8f152082834c19a8c1aa8
James <james@macbox1.local>
2015-08-08 11:23:42 +1000

    pingpong loop

--- MultiBlink/MultiBlink.ino
+++ MultiBlink/MultiBlink.ino
@@ -27,6 +27,13 @@ void loop() {
   digitalWrite(11, LOW);
   digitalWrite(13, HIGH);
   delay(1000);
+
+  digitalWrite(10, LOW);
+  digitalWrite(11, HIGH);
+  digitalWrite(13, LOW);
+  delay(1000);
+
+  
   
   
   
