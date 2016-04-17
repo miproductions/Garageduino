c20785997c19d68a13d6dd66adf4ae8a74cbd285
James <james@radsy.com>
2016-04-17 15:13:57 +1000

    coderdojo stuff

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,32 +1,39 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
 
-  Most Arduinos have an on-board LED you can control. On the Uno and
-  Leonardo, it is attached to digital pin 13. If you're unsure what
-  pin the on-board LED is connected to on your Arduino model, check
-  the documentation at http://www.arduino.cc
+#include "DHT.h"; // library for using the humidity and temperature sensor
 
-  This example code is in the public domain.
-
-  modified 8 May 2014
-  by Scott Fitzgerald
- */
-
+int lightSensorVal = 0;
+float temp = 0.0f; // stores the temperature
+float humi = 0.0f; // stores the humidity
+#define DHTTYPE DHT22 // needed for the humi/temp sensor
+DHT dht(3, DHTTYPE); // initialises the humi/temp sensor
+#define lightSensorAPinI 0 // reading light on 
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin 13 as an output.
-  pinMode(13, OUTPUT); // LED
-  pinMode(0), OUTPUT); // Relay 1
+  pinMode(13, OUTPUT); // onboard LED
+  pinMode(4, OUTPUT); // red led
+  pinMode(5, OUTPUT); // green led
+  Serial.begin(9600); // lets the arduino talk to the computer
+  dht.begin(); // starts the humi/temp sensor
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(0, HIGH);    // Relay 1 NO
-  delay(1000);              // wait for a second
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(0, LOW);    // Relay 1 NC
-  delay(1000);              // wait for a second
+  digitalWrite(13, HIGH); // turn on the onboard led
+  digitalWrite(4, LOW); // turn off the red led
+  digitalWrite(5, HIGH); // turn on the green led
+  delay(500); // wait half a second
+  digitalWrite(13, LOW); // turn off the onboard led
+  digitalWrite(4, HIGH); // turn on the red led
+  digitalWrite(5, LOW); // turn off the greed led
+  delay(500); // wait half a second
+  temp = dht.readTemperature(); // read the actual temperature from the sensor
+  Serial.print("Temperature:");
+  Serial.println(temp); // write the temp to the console
+  humi = dht.readHumidity(); // read the actual humidity from the sensor
+  Serial.print("Humidity:"); 
+  Serial.println(humi); // write the humidity to the console
+  lightSensorVal = analogRead(lightSensorAPinI);
+  Serial.print("Light Value:");
+  Serial.println(lightSensorVal);
 }
